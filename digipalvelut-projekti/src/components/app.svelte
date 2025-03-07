f9b8ef75a3e4dd16696d2c0ba0b0ac436e3b0fe0
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-03-07 12:40:08 +0200

    merge conflict solved

--- digipalvelut-projekti/src/components/app.svelte
+++ digipalvelut-projekti/src/components/app.svelte
@@ -12,20 +12,13 @@
     });
 
     setContext('list', lists);
-
-    console.log(lists.list)
 </script>
 
 <div>
     <AddNew />
     <Lines />
-<<<<<<< HEAD
     <Charts />
-=======
-    <!-- charts.svelte isn't working and causing errors so commented -->
-    <!-- <Charts /> -->
-     <FileReader/>
->>>>>>> fbe913113d960f7ea3e23d017b88f5682f2b8d80
+    <FileReader/>
 </div>
 
 <!-- font is added in here -->
