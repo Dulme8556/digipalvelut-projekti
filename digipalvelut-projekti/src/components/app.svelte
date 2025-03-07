87bfe554a3aeadd519e0b680b9859c1f43695937
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-03-07 13:08:33 +0200

    file reader moved to add-new.svelte

--- digipalvelut-projekti/src/components/app.svelte
+++ digipalvelut-projekti/src/components/app.svelte
@@ -4,7 +4,6 @@
     import AddNew from "./add-new.svelte";
     import Lines from "./lines.svelte";
     import Charts from "./charts.svelte";
-    import FileReader from "./file-reader.svelte";
     import GlobalStyles from "./global-styles.svelte";
 
     let lists = $state({
@@ -18,7 +17,6 @@
     <AddNew />
     <Lines />
     <Charts />
-    <FileReader/>
 </div>
 
 <!-- font is added in here -->
