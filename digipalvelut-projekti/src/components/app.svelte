b08de2627a70d96073f71c11a835f90b79ea202b
Miro Ihalainen <ihalainen.miro@student.jamk.fi>
2025-03-07 11:52:00 +0200

    Update

--- digipalvelut-projekti/src/components/app.svelte
+++ digipalvelut-projekti/src/components/app.svelte
@@ -4,19 +4,29 @@
     import AddNew from "./add-new.svelte";
     import Lines from "./lines.svelte";
     import Charts from "./charts.svelte";
-    import AddNewSpinBox from "./add-new-spin_box.svelte";
+    import FileReader from "./file-reader.svelte";
+    import GlobalStyles from "./global-styles.svelte";
 
     let lists = $state({
         list: [],
     });
 
     setContext('list', lists);
+
+    console.log(lists.list)
 </script>
 
 <div>
     <AddNew />
     <Lines />
+<<<<<<< HEAD
     <Charts />
+=======
+    <!-- charts.svelte isn't working and causing errors so commented -->
+    <!-- <Charts /> -->
+     <FileReader/>
+>>>>>>> fbe913113d960f7ea3e23d017b88f5682f2b8d80
 </div>
 
-<AddNewSpinBox />
+<!-- font is added in here -->
+<GlobalStyles />
