775a506025d21927e880af5c672eef190ce824e1
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-03-24 10:45:40 +0200

    fixed charts disappearing when updating indicators

--- digipalvelut-projekti/src/components/charts.svelte
+++ digipalvelut-projekti/src/components/charts.svelte
@@ -1,22 +1,22 @@
 <script>
     import { getContext, onMount } from "svelte";
     import Chart from "./chart.svelte"; // Import the Chart component
-    
+
     let chartsData = [];
     let chartId = 0;
-    
+
     let lists = getContext("list");
     let selected = lists.selectedValues;
-    
+
     let chartName = ""; // Chart name
     let chartNames = []; // Array to hold chart names
     let chartMade = false;
-    
+
     let datasetDataEnd = [];
     let datasetDataTarget = [];
     let datasetDataStart = [];
     let labels = [];
-    
+
     let typeOfChart = "bar";
     let chartTypes = [
         "bar",
@@ -28,26 +28,28 @@
         "radar",
         "scatter",
     ];
-    
+
     let modified = false;
-    
+
     onMount(async () => {
         lists.charts = lists.charts.filter((item) => !Array.isArray(item));
-        
+
         // detect if selected values have changed
         if (lists.charts.length > 0) {
             chartsData = []; // empty the chart data
             modified = true;
 
-            lists.charts.forEach(element => {
+            lists.charts.forEach((element) => {
                 listOfChartData = [
-                    ...listOfChartData, {
+                    ...listOfChartData,
+                    {
                         type: element.type,
                         labels: element.labels,
                         datasets: element.datasets,
-                    }
-                ]
+                    },
+                ];
             });
+            createAllCharts(false);
         }
     });
 
@@ -108,25 +110,36 @@
         },
     ];
 
-    function createAllCharts() {
-        listOfChartData[0].type = typeOfChart
-        listOfChartData.forEach((element) => {
-            generateCharts(element);
-        });
-        listOfChartData = defaultValues
+    function createAllCharts(createNew) {
+        if (createNew) {
+            if (
+                datasetDataEnd.length !== 0 ||
+                datasetDataTarget.length !== 0 ||
+                labels.length !== 0
+            ) {
+                listOfChartData[0].type = typeOfChart;
+                listOfChartData.forEach((element) => {
+                    generateCharts(element);
+                });
+                listOfChartData = defaultValues;
+            } else {
+                alert("Necessary data is missing.");
+                return;
+            }
+        } 
+        // ^^ createAllCharts is called from Create chart button so it also adds new chart
+        // vv else just load the old ones
+        else {
+            for (let i = 1; i < listOfChartData.length; i++) {
+                let element = listOfChartData[i];
+                generateCharts(element);
+            }
+            listOfChartData = defaultValues;
+        }
     }
 
     const generateCharts = (element) => {
-        if (
-            datasetDataEnd.length === 0 ||
-            datasetDataTarget.length === 0 ||
-            labels.length === 0
-        ) {
-            alert("Necessary data is missing.");
-            return;
-        }
-
-        element = JSON.parse(JSON.stringify(element))
+        element = JSON.parse(JSON.stringify(element));
 
         chartsData = [
             ...chartsData,
@@ -148,7 +161,6 @@
 
         lists.charts = chartsData; // replace lists.charts values with chartsData values
     };
-
 </script>
 
 <div>
@@ -166,24 +178,18 @@
             {/each}
         </select>
         <div class="chartButton">
-            <button class="chartButton__button" onclick={createAllCharts}>
+            <button class="chartButton__button" onclick={createAllCharts(true)}>
                 Create a chart
             </button>
         </div>
     </div>
-        <div>
-            {#each chartsData as data, i}
-            <div>{i}</div>
+    <div>
+        {#each chartsData as data, i}
             <div>
-                <Chart
-                id={i}
-                {data}
-                {chartMade}
-                chartName={chartNames[i]}
-                />
+                <Chart id={i} {data} {chartMade} chartName={chartNames[i]} />
             </div>
-            {/each}
-        </div>
+        {/each}
+    </div>
 </div>
 
 <style>
