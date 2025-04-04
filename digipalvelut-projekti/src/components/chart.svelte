97d02aeea0ee2b3ebc5cdae295e30d6bcdc71c48
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-04-04 11:53:41 +0300

    cleaned chart.svelte

--- digipalvelut-projekti/src/components/chart.svelte
+++ digipalvelut-projekti/src/components/chart.svelte
@@ -1,219 +1,231 @@
 <script>
-  import { getContext, onDestroy, onMount } from "svelte";
-  import Chart from "chart.js/auto";
-  import { jsPDF } from "jspdf";
+    import { getContext, onDestroy, onMount } from "svelte";
+    import Chart from "chart.js/auto";
+    import { jsPDF } from "jspdf";
 
-  let lists = getContext("list");
+    let lists = getContext("list");
 
-  export let data;
-  export let chartMade;
-  export let checkSelected;
-  let id;
-  let chartName;
+    export let data;
+    export let chartMade;
+    export let checkSelected;
+    let id;
+    let chartName;
 
-  let canvas;
-  let chartInstance;
-  let chartContainer;
+    let canvas;
+    let chartInstance;
+    let chartContainer;
 
-  let checked = false;
+    let checked = false;
 
-  onMount(() => {
-    if (!canvas) {
-      return;
-    }
+    onMount(() => {
+        if (!canvas) {
+            return;
+        }
 
-    id = data.id;
+        id = data.id;
 
-    if (data.title) {
-      chartName = data.title;
-    } else {
-      chartName = "";
-    }
-    
-    chartInstance = new Chart(canvas, {
-      type: data.type,
-      data,
-      options: {
-        responsive: false,
-        maintainAspectRatio: false,
-        animations: false,
-        indexAxis: data.indexAxis,
-      },
+        if (data.title) {
+            chartName = data.title;
+        } else {
+            chartName = "";
+        }
+
+        chartInstance = new Chart(canvas, {
+            type: data.type,
+            data,
+            options: {
+                responsive: false,
+                maintainAspectRatio: false,
+                animations: false,
+                indexAxis: data.indexAxis,
+            },
+        });
+
+        // Store the parent container of this chart
+        chartContainer = canvas.closest(".chartContainer");
+
+        checked = lists.charts[id - 1].check;
     });
 
-    // Store the parent container of this chart
-    chartContainer = canvas.closest(".chartContainer");
+    onDestroy(() => {
+        chartInstance?.destroy();
+    });
 
-    checked = lists.charts[id-1].check
-  });
+    function deleteChart() {
+        if (chartInstance) {
+            chartInstance.destroy();
+            chartInstance = null;
+            chartMade = false;
 
-  onDestroy(() => {
-    chartInstance?.destroy();
-  });
+            // Remove the parent container of the chart (the div that contains the canvas)
+            if (chartContainer) {
+                chartContainer.remove();
+            }
+            // ^^ set OnMount
 
-  function deleteChart() {
-    if (chartInstance) {
-      chartInstance.destroy();
-      chartInstance = null;
-      chartMade = false;
-
-      // Remove the parent container of the chart (the div that contains the canvas)
-      if (chartContainer) {
-        chartContainer.remove();
-      }
-      // ^^ set OnMount
-
-      lists.charts = lists.charts.filter((item) => item.id !== id);
-    }
-  }
-
-  function downloadPDF() {
-    if (!chartMade || !canvas) {
-      console.error("No chart available for download");
-      return;
+            lists.charts = lists.charts.filter((item) => item.id !== id);
+        }
     }
 
-    const chartWidth = canvas.width;
-    const chartHeight = canvas.height;
-    const doc = new jsPDF("l", "mm", [chartWidth, chartHeight]);
-    const imgData = canvas.toDataURL("image/png");
+    function downloadPDF() {
+        if (!chartMade || !canvas) {
+            console.error("No chart available for download");
+            return;
+        }
 
-    doc.addImage(imgData, "PNG", 0, 0, chartWidth, chartHeight);
+        const chartWidth = canvas.width;
+        const chartHeight = canvas.height;
+        const doc = new jsPDF("l", "mm", [chartWidth, chartHeight]);
+        const imgData = canvas.toDataURL("image/png");
 
-    const filename =
-      chartName && chartName.trim() !== "" ? `${chartName}.pdf` : "chart.pdf";
+        doc.addImage(imgData, "PNG", 0, 0, chartWidth, chartHeight);
 
-    doc.save(filename);
-  }
-  
-  function checkboxClick() {
-    checked = !checked;
-    lists.charts[id-1].check = checked;
-    checkSelected()
-  }
+        const filename =
+            chartName && chartName.trim() !== ""
+                ? `${chartName}.pdf`
+                : "chart.pdf";
 
-  export function checkAll() {
-    checked = true;
-  }
-  export function uncheckAll() {
-    checked = false;
-  }
-  
-  // to give needed data to the parent
-  export function returnChartInstance() {
-    return chartInstance;
-  }
-  export function returnCanvas() {
-    return canvas;
-  }
-  export function returnCheck() {
-    return checked;
-  }
-  export function returnId() {
-    return id;
-  }
+        doc.save(filename);
+    }
+
+    function checkboxClick() {
+        checked = !checked;
+        lists.charts[id - 1].check = checked;
+        checkSelected();
+    }
+
+    export function checkAll() {
+        checked = true;
+    }
+    export function uncheckAll() {
+        checked = false;
+    }
+    
+    // to give needed data to the parent
+    export function returnCanvas() {
+        return canvas;
+    }
+    export function returnCheck() {
+        return checked;
+    }
+    export function returnId() {
+        return id;
+    }
 </script>
 
 {#if chartMade}
-  <div class="chartContainer">
-    <div style="display:flex; justify-content:space-between">
-      <div
-        class="buttonContainer__chartName"
-        style="display:flex; font-size:larger; font-weight:700;">
-        {#if chartName == ""}
-          <p>*UNNAMED CHART*</p>
-        {:else}
-          {chartName}
-        {/if}
-      </div>
-      <div class="buttonContainer">
-        <input type="checkbox" class="chart-button" onclick={checkboxClick} {checked}/>
-        <button class="chart-button pdf-download__button" onclick={downloadPDF}>
-          <img
-            class="pdf-download__image"
-            src="./images/download-icon.svg"
-            alt=""
-          />
-        </button>
-        <button class="chart-button chart-delete__button" onclick={deleteChart}>
-          <img
-            class="chart-delete__image"
-            src="./images/delete-icon.svg"
-            alt=""
-          />
-        </button>
-      </div>
+    <div class="chartContainer">
+        <div style="display:flex; justify-content:space-between">
+            <div
+                class="buttonContainer__chartName"
+                style="display:flex; font-size:larger; font-weight:700;"
+            >
+                {#if chartName == ""}
+                    <p>*UNNAMED CHART*</p>
+                {:else}
+                    {chartName}
+                {/if}
+            </div>
+            <div class="buttonContainer">
+                <input
+                    type="checkbox"
+                    class="chart-button"
+                    onclick={checkboxClick}
+                    {checked}
+                />
+                <button
+                    class="chart-button pdf-download__button"
+                    onclick={downloadPDF}
+                >
+                    <img
+                        class="pdf-download__image"
+                        src="./images/download-icon.svg"
+                        alt=""
+                    />
+                </button>
+                <button
+                    class="chart-button chart-delete__button"
+                    onclick={deleteChart}
+                >
+                    <img
+                        class="chart-delete__image"
+                        src="./images/delete-icon.svg"
+                        alt=""
+                    />
+                </button>
+            </div>
+        </div>
+        <div id="content" style="width:400px; max-height:300px;">
+            <canvas style="width: 400px; max-height:300px;" bind:this={canvas}
+            ></canvas>
+        </div>
     </div>
-    <div id="content" style="width:400px; max-height:300px;">
-      <canvas style="width: 400px; max-height:300px;" bind:this={canvas}></canvas>
-    </div>
-  </div>
 {/if}
 
 <style>
-  .chartContainer {
-    width: 400px;
-    margin-bottom: 55px;
-  }
+    .chartContainer {
+        width: 400px;
+        margin-bottom: 55px;
+    }
 
-  .chartContainer:first-child {
-    margin-top: 25px;
-  }
+    .chartContainer:first-child {
+        margin-top: 25px;
+    }
 
-  .chart-button {
-    display: flex;
-    background-color: grey;
-    height: 30px;
-    width: 30px;
-    justify-self: flex-end;
-    border: 1px solid black;
-    border-radius: 3px;
-    padding: 0;
-  }
+    .chart-button {
+        display: flex;
+        background-color: grey;
+        height: 30px;
+        width: 30px;
+        justify-self: flex-end;
+        border: 1px solid black;
+        border-radius: 3px;
+        padding: 0;
+    }
 
-  .pdf-download__button {
-    margin-left: 5px;
-  }
+    .pdf-download__button {
+        margin-left: 5px;
+    }
 
-  .pdf-download__button:hover {
-    background-color: darkgray;
-    cursor: pointer;
-  }
+    .pdf-download__button:hover {
+        background-color: darkgray;
+        cursor: pointer;
+    }
 
-  .pdf-download__image {
-    display: flex;
-    width: 28px;
-    height: 27px;
-    filter: brightness(0) saturate(100%) invert(96%) sepia(8%) saturate(7%)
-      hue-rotate(314deg) brightness(89%) contrast(93%);
-  }
+    .pdf-download__image {
+        display: flex;
+        width: 28px;
+        height: 27px;
+        filter: brightness(0) saturate(100%) invert(96%) sepia(8%) saturate(7%)
+            hue-rotate(314deg) brightness(89%) contrast(93%);
+    }
 
-  .buttonContainer {
-    margin-right: 13px;
-    display: flex;
-    flex-direction: row;
-    justify-content: end;
-    align-items: center;
-  }
+    .buttonContainer {
+        margin-right: 13px;
+        display: flex;
+        flex-direction: row;
+        justify-content: end;
+        align-items: center;
+    }
 
-  .chart-delete__button {
-    margin-left: 5px;
-  }
+    .chart-delete__button {
+        margin-left: 5px;
+    }
 
-  .chart-delete__button:hover {
-    background-color: #e74433;
-    cursor: pointer;
-  }
+    .chart-delete__button:hover {
+        background-color: #e74433;
+        cursor: pointer;
+    }
 
-  .chart-delete__image {
-    display: flex;
-    align-self: center;
-    justify-self: center;
-    padding: 3px;
-  }
+    .chart-delete__image {
+        display: flex;
+        align-self: center;
+        justify-self: center;
+        padding: 3px;
+    }
 
-  .chart-delete__image:hover {
-    filter: brightness(0) saturate(100%) invert(59%) sepia(6%) saturate(18%)
-      hue-rotate(324deg) brightness(84%) contrast(94%);
-  }
+    .chart-delete__image:hover {
+        filter: brightness(0) saturate(100%) invert(59%) sepia(6%) saturate(18%)
+            hue-rotate(324deg) brightness(84%) contrast(94%);
+    }
 </style>
