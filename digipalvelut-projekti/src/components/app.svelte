95224ff842ab8216131a65f4fab53c66972b6f77
Tico Korhonen <tico.korhonen@gmail.com>
2025-04-15 10:52:09 +0300

    header (sketch, will probably be changed)

--- digipalvelut-projekti/src/components/app.svelte
+++ digipalvelut-projekti/src/components/app.svelte
@@ -17,6 +17,10 @@
 </script>
 
 <div class="website">
+    <div class="credit">
+        <img class="vr4vet_logo" src="./images/VR4VET_LOGO_DOTS_trnsprnt_Turquoise.png">
+        <img class="eu_union" src="./images/EN_FundedbytheEU_RGB_POS.png">
+    </div>
     <div class="main__functions">
         <div style='display:flex;'>
             <AddNew />
@@ -40,4 +44,47 @@
         flex-direction: column;
         justify-content: space-between;
     }
+
+    .credit,
+    .credit * {
+      box-sizing: border-box;
+    }
+    .credit {
+      display:flex;
+      align-items:center;
+      background: linear-gradient(
+       90deg,
+       rgba(41, 168, 175, 1) 0%,
+       rgba(17, 70, 73, 1) 100%
+      );
+      height: 130px;
+      position: relative;
+    }
+
+    .vr4vet_logo,
+    .vr4vet_logo * {
+      box-sizing: border-box;
+    }
+    .vr4vet_logo {
+      width: 220px;
+      margin-right:50px;
+      height: 100px;
+      position: relative;
+      object-fit: cover;
+      aspect-ratio: 220/100;
+    }
+
+    .eu_union,
+    .eu_union * {
+      box-sizing: border-box;
+    }
+    .eu_union {
+      height: 100px;
+      position: relative;
+      object-fit: cover;
+      aspect-ratio: 448.63/100;
+    }
+
+
+
 </style>
