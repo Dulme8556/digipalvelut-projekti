fa9a26dea53de63ca65cd748218399fc64719251
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-05-05 09:42:17 +0300

    fixed bug when opening excel file

--- digipalvelut-projekti/src/components/line.svelte
+++ digipalvelut-projekti/src/components/line.svelte
@@ -25,10 +25,14 @@
 
     onMount(() => {
         let today = new Date().toISOString().split('T')[0]
-        if (deadline !== today && responsibility !== "") {
-            extraData = true
-        } else {
-            extraData = false
+        // to detect when excel is opened
+        if (deadline && responsibility) {
+            // to detect when indicator made manually and values are empty
+            if (deadline !== today || deadline !== "" && responsibility !== "") {
+                extraData = true
+            } else {
+                extraData = false
+            }
         }
     });
 
