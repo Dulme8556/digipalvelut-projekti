0f4da82e44af663ff0f7fe03e7c349da551be391
Dulme8556 <111154640+Dulme8556@users.noreply.github.com>
2025-05-12 09:56:23 +0300

    multiple extra points on line fixed

--- digipalvelut-projekti/src/components/charts-section/chart-config/line.js
+++ digipalvelut-projekti/src/components/charts-section/chart-config/line.js
@@ -6,21 +6,37 @@ class Line {
     changeData(start, end, target, unit, labels, customFields) {
         let colorList = ["#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", "#9966FF"]
         let datasetList = [];
-        let customFieldsDataset = [];
         let convertedCustomFields = JSON.parse(JSON.stringify(customFields))
+        let customFieldsDataset = [];
         let customFieldValues = [];
         let customFieldLabels = [];
 
-        if (!convertedCustomFields.some(item => item == null)) {
-            customFieldValues = convertedCustomFields[0].map(item => Number(item.value));
-            customFieldLabels = convertedCustomFields[0].map(item => item.title);
+        // tämmöne tuli bar charttien muokkauksen jälkeen
+        // tän voi kopioida sinne toisen tilalle ja kattoo console.log() nii näkee miks ei oo enää
+        // convereteddCustomFields pitää vaihtaa eka
+        if (!convertedCustomFields.some(item => item === null)) {    
+            for (let i = 0; i < convertedCustomFields.length; i++) {
+                for (let j = 0; j < convertedCustomFields[i].length; j++) { 
+                    if (!customFieldsDataset[i]) customFieldsDataset[i] = []
+                    customFieldValues = convertedCustomFields[i].map(item => Number(item.value));
+                    customFieldLabels = convertedCustomFields[i].map(item => item.title);   
+                }
+                customFieldsDataset.push({values: customFieldValues, labels: customFieldLabels})
+            }
         }
-        
-        let labelList = ["start", ...customFieldLabels, "end", "target"]
+        // delete first slot from list which is empty
+        customFieldsDataset.shift();
+                
+        let labelList = ["start", ...customFieldLabels, "result", "expected"]
         let storeData = { start: start, end: end, target: target }
 
         for (let i = 0; i < labels.length; i++) {
-            let data = [storeData.start[i],...customFieldValues, storeData.end[i], storeData.target[i]]
+            let data = [
+                storeData.start[i],
+                ...(customFieldsDataset[i]?.values ?? []),
+                storeData.end[i],
+                storeData.target[i]
+            ];
             let dataset = { label: labels[i], data: data, backgroundColor: colorList[i], unit: unit[i]}
             datasetList.push(dataset)
         }
