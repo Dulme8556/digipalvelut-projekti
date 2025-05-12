4b64099d93e586ede88237f7fc4d22fefe595a46
Tico Korhonen <tico.korhonen@gmail.com>
2025-05-12 09:13:04 +0300

    line tests, onmount issue

---
+++ digipalvelut-projekti/src/components/lines-section/line.test.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/svelte';
+import { describe, it, expect, vi } from 'vitest';
+import line from './line.svelte';
+
+describe('line component', () => {
+  it('runs with correct props', () => {
+    const { getByText } = render(line, {
+      props: {
+        name: 'Test line',
+        expected: 100,
+        result: 50,
+        start: 0,
+        unit: '%',
+        check: false,
+        deadline: '',
+        responsibility: '',
+        function: vi.fn(),
+        // "mock" function, check if the correct function was called
+        update: vi.fn(),
+        id: 1
+      },
+      context: new Map([
+        ['list', { list: [], selectedValues: [] }]
+      ])
+    });
+
+    expect(getByText('Test line')).toBeTruthy();
+    expect(getByText('100')).toBeTruthy();
+    expect(getByText('50')).toBeTruthy();
+    expect(getByText('%')).toBeTruthy();
+  });
+});
---
+++ digipalvelut-projekti/src/components/lines-section/lines.test.js
@@ -0,0 +1,44 @@
+import { render, fireEvent, screen } from '@testing-library/svelte';
+import { describe, it, expect, vi } from 'vitest';
+import Lines from './lines.svelte';
+
+
+describe('Lines', () => {
+
+  it('should update searchquery when typing in the searchbar', async () => {
+    render(Lines, {
+      props: {
+        lists: {
+          selectedValues: [],
+          list: [{ id: 1, name: 'Line 1' }, { id: 2, name: 'Line 2' }]
+        }
+      }
+    });
+
+    const searchInput = screen.getByPlaceholderText('Search indicators...');
+    await fireEvent.input(searchInput, { target: { value: 'Line 1' } });
+
+    expect(searchInput.value).toBe('Line 1');
+  });
+
+
+
+  it('should call selectAll function when selectall button is clicked', async () => {
+    const selectAll = vi.fn();
+
+    render(Lines, {
+      props: {
+        lists: {
+          selectedValues: [],
+          list: [{ id: 1, name: 'Line 1' }, { id: 2, name: 'Line 2' }]
+        },
+        selectAll
+      }
+    });
+
+    const selectAllButton = screen.getByText('Select all');
+    await fireEvent.click(selectAllButton);
+
+    expect(selectAll).toHaveBeenCalled();
+  });
+});
