97d722e0521e884e745af74f4927092f5986f450
Tico Korhonen <tico.korhonen@gmail.com>
2025-05-20 11:44:56 +0300

    tried to fix, most tests nonexistant now because of onmount and config issues

--- digipalvelut-projekti/src/components/header/file-reader.test.js
+++ digipalvelut-projekti/src/components/header/file-reader.test.js
@@ -1,28 +1,28 @@
-// import { render, fireEvent, waitFor } from '@testing-library/svelte';
-// import { describe, it, expect, vi, beforeEach } from 'vitest';
-// import FileReader from './file-reader.svelte';
+import { render, fireEvent, waitFor } from '@testing-library/svelte';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+import FileReader from './file-reader.svelte';
 
-// describe('FileReader component', () => {
+describe('FileReader component', () => {
 
-//   it('adds item to list after file is uploaded', async () => {
-//     const { container } = render(FileReader);
-//     const input = container.querySelector('input[type="file"]');
+  it('adds item to list after file is uploaded', async () => {
+    const { container } = render(FileReader);
+    const input = container.querySelector('input[type="file"]');
 
-//     const fakeFile = new File([''], 'test.xlsx', {
-//       type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-//     });
+    const fakeFile = new File([''], 'test.xlsx', {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
 
-//     Object.defineProperty(fakeFile, 'arrayBuffer', {
-//       value: () => Promise.resolve(new ArrayBuffer(8))
-//     });
+    Object.defineProperty(fakeFile, 'arrayBuffer', {
+      value: () => Promise.resolve(new ArrayBuffer(8))
+    });
 
-//     await fireEvent.change(input, {
-//       target: { files: [fakeFile] },
-//     });
+    await fireEvent.change(input, {
+      target: { files: [fakeFile] },
+    });
 
-//     await waitFor(() => {
-//       expect(mockListContext.list.length).toBe(1);
-//       expect(mockListContext.list[0].name).toBe('Task A');
-//     });
-//   });
-// });
+    await waitFor(() => {
+      expect(mockListContext.list.length).toBe(1);
+      expect(mockListContext.list[0].name).toBe('Task A');
+    });
+  });
+});
